e39945409cb7f3c2bb14427ff8ae6ba4a581d72f
unknown <srbhsamir6@gmail.com>
2023-08-22 23:17:54 +0530

    attempt 1

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,12 @@
+package com.driver;
+
+public class B extends A {
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+
+    }
+    public void callParent(){
+        super.meth();
+    }
+
+}
